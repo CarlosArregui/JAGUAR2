e9a0dfd05fd7be45ad3bf4e5a5d2af926be8c61d
javiliebana <javierliebana99@gmail.com>
2019-05-03 19:14:04 +0200

    creacion de puntos y subirlos a firebase

--- app/src/main/java/com/jagoar/jaguar2/MainActivity.java
+++ app/src/main/java/com/jagoar/jaguar2/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent I = new Intent(contexto,MapsActivity.class);
                 startActivity(I);
+                finish();
             }
         });
     }
