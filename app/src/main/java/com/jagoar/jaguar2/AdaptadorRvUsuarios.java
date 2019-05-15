33df4f783939f991ac34841fd7ac7b7c1f01945a
CarlosArregui <carregui98@gmail.com>
2019-05-15 13:22:43 +0200

    commit pantalla buscar, recuadro de entidades

---
+++ app/src/main/java/com/jagoar/jaguar2/AdaptadorRvUsuarios.java
@@ -0,0 +1,127 @@
+package com.jagoar.jaguar2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AdaptadorRvUsuarios extends RecyclerView.Adapter<AdaptadorRvUsuarios.ListaUsuariosHolder> implements InterfazClickRV {
+    @NonNull
+    static List<Usuario> lista_usuarios_recy;
+    Context contexto;
+    private static InterfazClickRV itemListener;
+    private View.OnClickListener listener;
+    public AdaptadorRvUsuarios(List<Usuario> lista_usuarios) {
+        this.lista_usuarios_recy=lista_usuarios;
+    }
+
+
+    @Override
+    public AdaptadorRvUsuarios.ListaUsuariosHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rv_vista_add,viewGroup, false);
+
+        // viewGroup.setOnClickListener(this);
+        AdaptadorRV.ListaPuntosHolder usuarios = new AdaptadorRV.ListaPuntosHolder(v);
+        return usuarios;
+    }
+    View.OnClickListener oyente=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        }
+    };
+
+    @Override
+    public void onBindViewHolder(@NonNull AdaptadorRvUsuarios.ListaUsuariosHolder listaUsuariosHolder, int i) {
+        Usuario usuario =lista_usuarios_recy.get(i);
+        listaUsuariosHolder.tv_titulo_re.setText(punto.getTitulo());
+        listaUsuariosHolder.tv_fecha.setText(punto.getFecha());
+        listaUsuariosHolder.i=i;
+        // listaPuntosHolder.const_lay.setOnClickListener(oyente);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista_usuarios_recy.size();
+    }
+
+    @Override
+    public void recyclerViewListClicked(View v, int position) {
+
+    }
+
+
+    public static class ListaUsuariosHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView tv_titulo_re, tv_fecha;
+        ImageView imagen;
+        int i;
+
+        Button btn_abrir;
+        ConstraintLayout const_lay;
+        public ListaUsuariosHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_titulo_re=itemView.findViewById(R.id.tv_titulo);
+            tv_fecha=itemView.findViewById(R.id.tv_fecha);
+
+            const_lay=(ConstraintLayout)itemView.findViewById(R.id.constraint_lay);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            sacarAlertDialog(lista_usuarios_recy.get(this.getPosition()), v );
+
+        }
+    }
+    public static void sacarAlertDialog(Punto punto, View v)
+    {
+        // Log.v("clicado", "posciion:"+position);
+
+        // Evento evento=lista_eventos_recy.get(position);
+
+        Log.v("clicado","Clase:"+ v.getClass());
+        AlertDialog.Builder constructor= new AlertDialog.Builder(v.getContext());
+
+        LayoutInflater inflador=LayoutInflater.from(v.getContext());
+        final View vista=inflador.inflate(R.layout.alert_di_recy,null);
+        constructor.setView(vista);
+
+        TextView tv_titulo= vista.findViewById(R.id.tv_titulo);
+
+        TextView tv_fecha_hora= vista.findViewById(R.id.tv_fecha);
+        TextView tv_descripcion= vista.findViewById(R.id.tv_autor);
+
+
+        tv_titulo.setText(punto.getTitulo());
+        tv_descripcion.setText(punto.getCreador());
+
+        tv_fecha_hora.setText(punto.getFecha());
+
+        constructor.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("ALERT","has clicado aceptar");
+            }
+        });
+        constructor.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("ALERT","has clicado cancelar");
+
+            }
+        });
+        AlertDialog alert=constructor.create();
+        alert.show();
+    }
+}
---
+++ app/src/main/java/com/jagoar/jaguar2/BuscarFragment.java
@@ -0,0 +1,76 @@
+package com.jagoar.jaguar2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class BuscarFragment extends Fragment {
+
+    RecyclerView rv;
+    List<Usuario> usuarios;
+    AdaptadorRvUsuarios adapter;
+    Context contexto;
+    String current_user;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+
+    }
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        if (getArguments() != null) {
+            current_user = getArguments().getString("currentUser");
+            Log.v("jeje",current_user);
+        }
+        rv = getView().findViewById(R.id.recycler_home);
+        rv.setLayoutManager(new LinearLayoutManager(contexto));
+        usuarios = new ArrayList<>();
+
+
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+        DatabaseReference bbdd = FirebaseDatabase.getInstance().getReference("puntos");
+        Query q=bbdd.orderByChild("creador").equalTo(current_user);
+        q.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            //saca datos y los catualiza en la vista
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usuarios.removeAll(usuarios);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Usuario user = snapshot.getValue(Usuario.class);
+                    usuarios.add(user);
+                }
+                adapter = new AdaptadorRV(usuarios);
+                adapter.notifyDataSetChanged();
+                rv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
