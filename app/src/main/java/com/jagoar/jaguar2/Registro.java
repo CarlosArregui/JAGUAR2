98b76f64346bcf3a23ba5d315bbac776e16ac392
gonza <gonzalo224455@gmail.com>
2019-06-09 18:11:46 +0200

    Registro diiooooooooooooooooossssssssssss

--- app/src/main/java/com/jagoar/jaguar2/Registro.java
+++ app/src/main/java/com/jagoar/jaguar2/Registro.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -35,7 +36,7 @@ import com.google.firebase.database.ValueEventListener;
  * Este activity es el del registro
  */
 public class Registro extends AppCompatActivity {
-    private LinearLayout layoutSnack;
+    private ConstraintLayout layoutSnack;
     private EditText email;
     private EditText pass,et_usuario;
     private EditText passConfirm;
